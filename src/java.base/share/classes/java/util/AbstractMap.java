c84d0dfe1b758c8a35a46b7d6f3d27f137d0835f
Michael Ernst <mernst@cs.washington.edu>
2026-05-21 18:21:24 -0700

    Fix bad merge

--- src/java.base/share/classes/java/util/AbstractMap.java
+++ src/java.base/share/classes/java/util/AbstractMap.java
@@ -439,7 +439,6 @@ public abstract class AbstractMap<K,V> implements Map<K,V> {
 
                 @Pure
                 @EnsuresNonEmptyIf(result = true, expression = "this")
-                @Pure
                 public boolean contains(@UnknownSignedness Object k) {
                     return AbstractMap.this.containsKey(k);
                 }
